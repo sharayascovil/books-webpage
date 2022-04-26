c03a66fcb179b2c221f9b5bae5cdb3bd7f4d8dce
ShaRaya <sharayascovil@u.northwestern.edu>
2022-04-26 13:10:41 -0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-books.ts
@@ -0,0 +1,16 @@
+import { Book } from './book';
+
+export const BOOKS: Book[] = [
+  { author: 'Delia Owens', title: 'Where the Crawdads Sing', genre: 'Literary Fiction', published: 2018},
+  { author: 'Betty Smith', title: 'A Tree Grows in Brooklyn', genre: 'Domestic Fiction', published: 1943 },
+  { author: 'Charles Dickens', title: 'Great Expectations', genre: 'Gothic Fiction', published: 1861},
+  { author: 'Octavia Butler', title: 'Kindred', genre: 'Sci-fi', published: 1979},
+  { author: 'Stephen King', title: 'The Institute', genre: 'Horror', published: 2019},
+  { author: 'Sy Montgomery', title: 'The Soul of an Octopus: A Surprising Exploration into the Wonder of Consciousness', genre: 'Nonfiction', published: 2016},
+  { author: 'Christina Lauren', title: 'The Unhoneymooners', genre: 'Romance', published: 2019},
+  { author: 'Tara Westover', title: 'Educated', genre: 'Biography', published: 2018},
+  { author: 'Michelle Obama', title: 'Becoming', genre: 'Biography', published: 2018},
+  { author: 'Madeline Miller', title: 'Circe', genre: 'Fantasy Fiction', published: 2018},
+  { author: 'Kristin Hannah', title: 'The Nightingale', genre: 'Historical Fiction', published: 2015},
+  {author: 'Cormac McCarthy', title: 'The Road', genre: 'Tragedy', published: 2006}
+];
